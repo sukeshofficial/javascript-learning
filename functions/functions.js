bd2f42f0f2f4e58a2a1ab7dbe7a68001d451d27c
sukeshofficial <sukesh.official.2006@gmail.com>
2025-04-30 12:02:45 +0530

    Initial Commit

---
+++ functions/functions.js
@@ -0,0 +1,13 @@
+const sayHello = (name) => "Hello, " + name + "!";
+
+console.log(sayHello("Sukesh"));
+
+const tripleNumber = (num) => 3 * num;
+
+console.log(tripleNumber(10));
+
+const check = (word) => word[0] === "S";
+
+let name = "Sukesh";
+
+console.log(check(name));
